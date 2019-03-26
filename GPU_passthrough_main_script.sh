7d75ba85ef8b1e83cde9fe89f0a18a6fac804919
alttehn <31592612+alttehn@users.noreply.github.com>
2019-03-26 22:56:43 +0300

    Add files via upload

--- GPU_passthrough_main_script.sh
+++ GPU_passthrough_main_script.sh
@@ -2,12 +2,14 @@
 # Script for auto configure passthrouh
 #to run in Linux root under root user:  
 # curl -o- https://raw.githubusercontent.com/alttehn/Proxmox-ServerSetup/master/GPU_passthrough_main_script.sh | bash
+# sh -c "$(curl -sSL https://raw.githubusercontent.com/alttehn/Proxmox-ServerSetup/master/GPU_passthrough_main_script.sh)"
 echo "Welcome to PCI GPU passthrough script! \n Before we start enable in your BIOS: VT-d:Enable, Intel Vitrualization Technology:Enable, Primary Graphx adapter:VGA, Above 4G Decoding:Enable."
-echo «xxxx»
-read Nothing
-echo «xxxx»
 read -p "Please press [Enter] to continue..."
-echo «xxxx»
+
+# File to Work with on Step 1. Processor»
+file='/etc/default/grub'
+
+# Argument for choice
 choice=4
 # Choise
 echo «What is the manufactory of your CPU?»
@@ -19,5 +21,55 @@ echo -n «Please choose 1 or 2 ? »
 while [ $choice -eq 4 ]; do
 
 read choice
+# if
+if [ $choice -eq 1 ] ; then
 
+echo «You have chosen Intel Processor»
+sed -i "s/quiet/quiet intel_iommu=on/" $file
+
+else
+
+if [ $choice -eq 2 ] ; then
+echo «You have chosen AMD Processor»
+sed -i "s/quiet/quiet amd_iommu=on/" $file
+else
+
+if [ $choice -eq 3 ] ; then
+choice=4
+else
+echo «What is the manufactory of your CPU?»
+echo «1. Intel Processor»
+echo «2. AMD Processor»
+echo -n «Please choose 1 or 2 ? »
+choice=4
+fi
+fi
+fi
+done
+#Apply Update to grub file
+update-grub
+#=============================================
+#Add GPU driver for blacklist in main system
+echo "vfio" >> /etc/resolv.conf
+echo "vfio_iommu_type1" >> /etc/resolv.conf
+echo "vfio_pci" >> /etc/resolv.conf
+echo "vfio_virqfd" >> /etc/resolv.conf
+#=============================================
+# get vfio-pci ids
+lspci -nn|grep -iP "NVIDIA|Radeon"
+echo ""
+echo ""
+echo "Please enter your vfio-pci ids, no quotes (e.g. '10de:1234,10de:4321')"
+read VFIOID
+echo options vfio-pci ids=$VFIOID > /etc/modprobe.d/vfio.conf
+echo ""
+echo "Your new /etc/modprobe.d/vfio.conf looks like this..."
+echo "-----------"
+cat /etc/modprobe.d/vfio.conf
+echo ""
+
+=================================================
+read -p "Script is over. Please press [Enter] to reboot"
+reboot
+echo "done"
 
