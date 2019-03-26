f08fc99e9fe9e73225e8328dba81920bbb08bc39
alttehn <31592612+alttehn@users.noreply.github.com>
2019-03-26 22:48:04 +0300

    Add files via upload

--- GPU_passthrough_main_script.sh
+++ GPU_passthrough_main_script.sh
@@ -2,7 +2,19 @@
 # Script for auto configure passthrouh
 #to run in Linux root under root user:  
 # curl -o- https://raw.githubusercontent.com/alttehn/Proxmox-ServerSetup/master/GPU_passthrough_main_script.sh | bash
-echo "Welcome to PCI GPU passthrough script! \r Before we start enable in your BIOS: VT-d:Enable, Intel Vitrualization Technology:Enable, Primary Graphx adapter:VGA, Above 4G Decoding:Enable."
+echo "Welcome to PCI GPU passthrough script! \n Before we start enable in your BIOS: VT-d:Enable, Intel Vitrualization Technology:Enable, Primary Graphx adapter:VGA, Above 4G Decoding:Enable."
 read -p "Please press [Enter] to continue..."
+
+choice=4
+# Choise
+echo «What is the manufactory of your CPU?»
+echo «1. Intel Processor»
+echo «2. AMD Processor»
+echo -n «Please choose 1 or 2 ? »
+# Waiting
+# Зацикливание
+while [ $choice -eq 4 ]; do
+
+read choice
 
 
